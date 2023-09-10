9882b436fcb3978e98c14478f47e8c6ba2e729d6
wasuken <wevorence@gmail.com>
2023-09-10 22:02:42 +0900

    バグだらけオセロ

--- src/Othello.tsx
+++ src/Othello.tsx
@@ -15,14 +15,64 @@ const Othello: React.FC = () => {
 
   const [board, setBoard] = useState<Cell[][]>(initialBoard);
   const [turn, setTurn] = useState<Cell>("black");
+  const searchStones = (
+    x: number,
+    y: number,
+    xx: number,
+    yy: number
+  ): boolean => {
+    // 範囲外
+    if (!board[x + xx] || !board[x + xx][y + yy]) return false;
+    const rstone = turn === "black" ? "white" : "black";
+    const v = board[x + xx][y + yy];
+    if (v) {
+      if (v === rstone) {
+        // 次を検索
+        return searchStones(x + xx, y + yy, xx, yy);
+      } else if (v === turn) {
+        return true;
+      }
+    }
+    return false;
+  };
+  const replaceStones = (x: number, y: number, xx: number, yy: number) => {
+    const nboard = [...board];
+    if (searchStones(x, y, xx, yy)) {
+      nboard[x][y] = turn;
+      for (let i = 1; board[x + xx * i][y + yy * i] !== turn; i++) {
+        nboard[x + xx * i][y + yy * i] = turn;
+      }
+      setBoard(nboard);
+    } else {
+      return false;
+    }
+    return true;
+  };
 
   const handleClick = (x: number, y: number) => {
     const nboard = [...board];
+    const rstone = turn === "black" ? "white" : "black";
+    let rst = false;
     if (nboard[x] && nboard[x][y]) {
-      nboard[x][y] = turn;
+      for (let i = -1; i < 2; i++) {
+        for (let k = -1; k < 2; k++) {
+          if (
+            !(i == 0 && k == 0) &&
+            nboard[x + i] &&
+            nboard[x + i][y + k] &&
+            nboard[x + i][y + k] === rstone
+          ) {
+            const r = replaceStones(x, y, i, k);
+            rst = rst || r;
+          }
+        }
+      }
     }
-    setTurn(turn === "black" ? "white" : "black");
-    setBoard(nboard);
+    if (rst) {
+      setTurn(rstone);
+    } else {
+      alert("置換失敗");
+    }
   };
 
   return (
